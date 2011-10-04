b144b751bfa0d387a0fc2930e5f81a441b760e99
Alexander Morland <alek@redpill-linpro.com>
2011-10-04 10:00:53 +0200

    added test to base managager

--- Tests/Cases/Manager/BaseManagerTest.php
+++ Tests/Cases/Manager/BaseManagerTest.php
@@ -5,12 +5,20 @@ namespace RedpillLinpro\GamineBundle\Tests\Cases\Manager;
 use RedpillLinpro\GamineBundle\Manager\BaseManager;
 
 class BaseManagerMock extends BaseManager {
-    
+
 }
 
 class BaseManagerTest extends \PHPUnit_Framework_TestCase
 {
+
+    private $man = null;
+
     public function testConstruction()
     {
+        $gamine_service = null;
+        $entity_key = 'Basic';
+        $access_service = null;
+        $this->man = new BaseManagerMock($gamine_service, $entity_key, $access_service);
+        $this->assertTrue($this->man instanceof BaseManager);
     }
 }
