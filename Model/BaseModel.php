d4732d7ad4b56b3e838d5d2a8e6478d05a37a11d
Daniel André Eikeland <zegenie@gmail.com>
2011-11-30 10:11:18 +0100

    properly unset id if NULL

--- Model/BaseModel.php
+++ Model/BaseModel.php
@@ -476,8 +476,11 @@ abstract class BaseModel implements StorableObjectInterface
                                 if (!count($diff)) {
                                     $result[$result_key][$k] = array($pk => $sub_model->getDataArrayIdentifierValue());
                                 } else {
-                                    if ($sub_model->getDataArrayIdentifierValue())
+                                    if ($sub_model->getDataArrayIdentifierValue()) {
                                         $diff[$pk] = $sub_model->getDataArrayIdentifierValue();
+                                    } else {
+                                        unset($diff[$pk]);
+                                    }
 
                                     $result[$result_key][$k] = $diff;
                                 }
